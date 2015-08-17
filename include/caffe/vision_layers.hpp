7e9619dd800c5ff3f03e2831c0b96f3f93c52f79
skylian <lianxiaochen@gmail.com>
2015-08-17 02:17:29 -0700

    spatial product layer

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -454,6 +454,47 @@ class CuDNNPoolingLayer : public PoolingLayer<Dtype> {
 #endif
 
 /**
+ * @brief bottom[0] * bottom[1] across channels
+ *       bottom[0] dim: NxCxHxW
+ *       bottom[1] dim: Nx1xHxW
+ *       top[0]    dim: NxCxHxW where each channel is the spatial prodcut between
+ *                      C-th channel of bottom[0] and bottom[1]
+ */
+template <typename Dtype>
+class SpatialProductLayer : public Layer<Dtype> {
+ public:
+  explicit SpatialProductLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual inline const char* type() const { return "SpatialProduct"; }
+
+  virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  int num_;
+  int channels_;
+  int height_;
+  int width_;
+
+  Blob<Dtype> multiplier_;  // dot multiplier for backward computation of params
+  Blob<Dtype> backward_buff_;  // temporary buffer for backward computation
+};
+
+/**
  * @brief Does spatial pyramid pooling on the input image
  *        by taking the max, average, etc. within regions
  *        so that the result vector of different sized
@@ -464,6 +505,10 @@ class SPPLayer : public Layer<Dtype> {
  public:
   explicit SPPLayer(const LayerParameter& param)
       : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
 
   virtual inline const char* type() const { return "SPP"; }
   virtual inline int ExactNumBottomBlobs() const { return 1; }
@@ -513,10 +558,6 @@ class SPPLayer : public Layer<Dtype> {
   vector<Blob<Dtype>*> concat_bottom_vec_;
   /// the internal Concat layers that the Flatten layers feed into
   shared_ptr<ConcatLayer<Dtype> > concat_layer_;
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 };
 
 }  // namespace caffe
